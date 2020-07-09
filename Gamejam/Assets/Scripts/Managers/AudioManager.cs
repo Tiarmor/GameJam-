8dc48980f73f82663b055860abd482382518e7ad
RefmBiox <67907392+RefmBiox@users.noreply.github.com>
2020-07-10 00:22:27 +0800

    Syj
    UI改动

---
+++ Gamejam/Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    //分别控制背景音乐和音效
+    AudioSource musicController;
+    AudioSource soundController;
+    int vol;
+
+    void Awake()
+    {
+        AudioSource[] sources = this.GetComponents<AudioSource>();
+        musicController = sources[0];
+        soundController = sources[1];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vol = PlayerPrefs.GetInt("Volume");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //选择BGM播放
+    public void MusiePlay(string musicName)
+    {
+        musicController.Stop();
+        musicController.clip = Resources.Load<AudioClip>("Audio/Music/" + musicName);
+        musicController.Play();
+    }
+
+    //选择音效播放
+    public void SoundPlay(string soundName)
+    {
+        soundController.Stop();
+        soundController.clip = Resources.Load<AudioClip>("Audio/Music/" + soundName);
+        soundController.Play();
+    }
+
+}
